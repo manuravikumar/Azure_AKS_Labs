b665d8735a1b1d5628802c7b46765abbc0497e65
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-02-12 17:35:29 +1100

    Refactor SSH key management to use azurerm_ssh_public_key and tls_private_key resources, updating outputs for public and private keys

--- AKS/ssh.tf
+++ AKS/ssh.tf
@@ -1,24 +1,30 @@
-resource "random_pet" "ssh_key_name" {
-  prefix    = "ssh"
-  separator = ""
+resource "azurerm_ssh_public_key" "example" {
+  name                = random_pet.ssh_key_name.id # You can still use random_pet for the name
+  resource_group_name = azurerm_resource_group.rg.name # Use the name property
+  location            = azurerm_resource_group.rg.location
+  public_key          = tls_private_key.example.public_key_openssh # Use the public key from tls_private_key
 }
 
-resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
-
-  response_export_values = ["publicKey", "privateKey"]
+resource "tls_private_key" "example" {
+ algorithm = "RSA"
 }
 
-resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id
-  location  = azurerm_resource_group.rg.location
-  parent_id = azurerm_resource_group.rg.id
+output "public_key_pem" {
+  value     = tls_private_key.example.public_key_pem
+  sensitive = true
 }
 
-output "key_data" {
-  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+output "private_key_pem" {
+  value     = tls_private_key.example.private_key_pem
+  sensitive = true
+}
+
+output "public_key_openssh" {
+  value     = tls_private_key.example.public_key_openssh
+  sensitive = true
+}
+
+output "private_key_openssh" {
+  value     = tls_private_key.example.private_key_openssh
+  sensitive = true
 }
