4c52c10a71a02af28a088e7601296c3a5ad36030
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-02-12 18:15:38 +1100

    Refactor SSH key management to remove random_pet resource and streamline key pair generation using azapi_resource_action

--- AKS/ssh.tf
+++ AKS/ssh.tf
@@ -1,8 +1,3 @@
-resource "random_pet" "ssh_key_name" {
-  prefix    = "ssh"
-  separator = ""
-}
-
 resource "azapi_resource" "ssh_public_key" {
   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   name      = random_pet.ssh_key_name.id
@@ -11,16 +6,14 @@ resource "azapi_resource" "ssh_public_key" {
 }
 
 resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = azapi_resource.ssh_public_key.type
+  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   resource_id = azapi_resource.ssh_public_key.id
   action      = "generateKeyPair"
   method      = "POST"
-
   response_export_values = ["publicKey", "privateKey"]
-
-  depends_on = [azapi_resource.ssh_public_key]  # Ensure SSH public key is created first
 }
 
 output "key_data" {
   value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
 }
+
