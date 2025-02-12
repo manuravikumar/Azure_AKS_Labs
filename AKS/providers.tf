d3b14f3099c0418257573dc4549972b26c5f22bf
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-02-12 19:36:01 +1100

    Add remote backend configuration for Terraform in AKS providers

--- AKS/providers.tf
+++ AKS/providers.tf
@@ -19,6 +19,13 @@ terraform {
       version = "0.9.1"
     }
   }
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "manuravikumar"
+    workspaces {
+      name = "aks-lab"
+    }
+  }
 }
 
 provider "azurerm" {
