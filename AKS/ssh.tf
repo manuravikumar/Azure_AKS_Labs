e59c4660f26b6bb0c5a2e4e9dcc04c84e4470c2a
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-02-14 17:00:26 +1100

    Add dependency for SSH public key generation on the public key resource

--- AKS/ssh.tf
+++ AKS/ssh.tf
@@ -6,7 +6,7 @@ resource "random_pet" "ssh_key_name" {
 # Create the SSH public key resource
 resource "azapi_resource" "ssh_public_key" {
   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id  # Use the random name from random_pet
+  name      = random_pet.ssh_key_name.id
   location  = azurerm_resource_group.rg.location
   parent_id = azurerm_resource_group.rg.id
 }
@@ -18,4 +18,6 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
   action      = "generateKeyPair"
   method      = "POST"
   response_export_values = ["publicKey", "privateKey"]
+
+  depends_on = [azapi_resource.ssh_public_key]
 }
