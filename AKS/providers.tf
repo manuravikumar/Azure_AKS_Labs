2c66fbeae594997033c0c5d599448952199b6e9a
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-02-17 17:08:05 +1100

    Remove Azure Service Principal authentication variables from provider and variables configuration

--- AKS/providers.tf
+++ AKS/providers.tf
@@ -21,18 +21,5 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-}
-
 provider "azapi" {
-  client_id       = var.AZURE_CLIENT_ID
-  client_secret   = var.AZURE_CLIENT_SECRET
-  tenant_id       = var.AZURE_TENANT_ID
-  subscription_id = var.AZURE_SUBSCRIPTION_ID
 }
